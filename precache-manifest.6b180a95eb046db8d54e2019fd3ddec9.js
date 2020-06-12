3aae27206bb3938c1b765a67b4141e301db24a36
federicoig <federicoignacio1@gmail.com>
2020-06-12 20:19:48 -0300

    Updates

---
+++ precache-manifest.6b180a95eb046db8d54e2019fd3ddec9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a9ab0f89ba7703447239b89e87174b",
+    "url": "/tambler/index.html"
+  },
+  {
+    "revision": "e4a8306bded9a4d156b2",
+    "url": "/tambler/static/css/main.2612ab81.chunk.css"
+  },
+  {
+    "revision": "5bee2aa20c325f8ae3b9",
+    "url": "/tambler/static/js/2.28d2fd14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tambler/static/js/2.28d2fd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a8306bded9a4d156b2",
+    "url": "/tambler/static/js/main.cb101c05.chunk.js"
+  },
+  {
+    "revision": "3683891054ebd13c87d5",
+    "url": "/tambler/static/js/runtime-main.d45daee7.js"
+  },
+  {
+    "revision": "11caa60be5f573f5e5e1a3f72becbea6",
+    "url": "/tambler/static/media/calendar.11caa60b.svg"
+  },
+  {
+    "revision": "6c7351652456d062fd9f469fc1bdd6ba",
+    "url": "/tambler/static/media/comments.6c735165.svg"
+  },
+  {
+    "revision": "0df7f2e83ac1c1f01d37c0c36c7474d6",
+    "url": "/tambler/static/media/like.0df7f2e8.svg"
+  },
+  {
+    "revision": "bdcf810afdda2eea1b1b01b125ff7262",
+    "url": "/tambler/static/media/repost.bdcf810a.svg"
+  },
+  {
+    "revision": "eaf31eb9bd6e7a2853ef5596ef7c2d68",
+    "url": "/tambler/static/media/ubication.eaf31eb9.svg"
+  }
+]);
